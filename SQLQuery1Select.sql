55ac9b852aa5a0170c9d47d0615d33954b39eafb
Elthon <eltonsilva.mecatronic@gmail.com>
2022-06-07 17:16:42 -0300

    Query 1

---
+++ SQLQuery1Select.sql
@@ -0,0 +1,34 @@
+ /* SELECT
+ --SQL Server, Postegres, Oracle, mySQL
+
+ SELECT coluna1, coluna2
+ FROM tabela
+
+ SELECT * 
+ FROM tabela */ 
+
+
+ SELECT *
+ FROM Person.Person;
+
+ SELECT Title
+ FROM Person.Person
+
+ SELECT *
+ from Person.EmailAddress
+
+ --DESAFIO--
+ /* A EQUIPE DE MARQUETING PRECISA DE FAZER UMA PESQUISA SOBRE NOMES MAIS COMUNS 
+ DE SEUS CLIENTES E PRECISA DO NOME E SOBRENOME DE TODOS OS CLIENTES QUE ESTÃO
+ CADASTRADOS NO SISTEMA. 
+ */
+
+ SELECT FirstName, LastName
+ FROM Person.Person;
+
+ --DESAFIO--
+ /* QUANTOS SOBRENOMES UNICOS TEMOS EM NOSSA TABELA PERSON.PERSON?
+ */
+
+ SELECT DISTINCT LastName
+ FROM Person.Person;
